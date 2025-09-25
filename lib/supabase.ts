14830730b57e6b5972f4af51406425ac0e62a30c
JavierTaborda <javiert.develop@gmail.com>
2025-09-25 11:39:32 -0400

    refact: web support and config

--- lib/supabase.ts
+++ lib/supabase.ts
@@ -2,14 +2,18 @@ import AsyncStorage from "@react-native-async-storage/async-storage";
 import { createClient } from "@supabase/supabase-js";
 import * as aesjs from 'aes-js';
 import * as SecureStore from 'expo-secure-store';
-import { AppState } from 'react-native';
+import { AppState, Platform } from 'react-native';
 import 'react-native-get-random-values';
 import 'react-native-url-polyfill/auto';
 
 //TODO config env for eas build
 const supabaseUrl = process.env.EXPO_PUBLIC_SUPABASE_URL!;
 const supabaseAnonKey = process.env.EXPO_PUBLIC_SUPABASE_ANON_KEY!;
-// Custom storage layer using AES encryption
+
+//for web :
+const isWeb = Platform.OS === "web";
+
+// for mobile: Custom storage layer using AES encryption + SecureStore + AsyncStorage
 class CustomSecureStore {
   private async _encrypt(key: string, value: string) {
     const encryptionKey = crypto.getRandomValues(new Uint8Array(256 / 8)); // AES-256
@@ -48,21 +52,43 @@ class CustomSecureStore {
   }
 }
 
+// for Web use localStorage
+const WebStorage = {
+  getItem: async (key: string) => {
+    if (typeof window !== "undefined" && window.localStorage) {
+      return window.localStorage.getItem(key);
+    }
+    return null;
+  },
+  setItem: async (key: string, value: string) => {
+    if (typeof window !== "undefined" && window.localStorage) {
+      window.localStorage.setItem(key, value);
+    }
+  },
+  removeItem: async (key: string) => {
+    if (typeof window !== "undefined" && window.localStorage) {
+      window.localStorage.removeItem(key);
+    }
+  },
+};
+
 export const supabase = createClient(supabaseUrl, supabaseAnonKey, {
   auth: {
-    storage: new CustomSecureStore,
+    storage: isWeb ? WebStorage : new CustomSecureStore(),
     autoRefreshToken: true,
     persistSession: true,
-    detectSessionInUrl: false,
+    detectSessionInUrl: isWeb,
 
   },
 });
 
-
-AppState.addEventListener('change', (state) => {
-  if (state === 'active') {
-    supabase.auth.startAutoRefresh()
-  } else {
-    supabase.auth.stopAutoRefresh()
-  }
-})
+//App state  in mobile
+if (!isWeb) {
+  AppState.addEventListener("change", (state) => {
+    if (state === "active") {
+      supabase.auth.startAutoRefresh();
+    } else {
+      supabase.auth.stopAutoRefresh();
+    }
+  });
+}
